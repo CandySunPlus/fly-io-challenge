12822bbffb33dc8ad05f0d56749b3a3f78662e02
sunbeiluo <sunbeiluo@meituan.com>
2023-12-01 18:18:02 +0800

    add uniqe-ids binary

--- src/bin/uniqe-ids.rs
+++ src/bin/uniqe-ids.rs
@@ -1,3 +1,49 @@
-fn main() {
-    unimplemented!();
+use std::io::StdoutLock;
+
+use rustrom::main_loop;
+use rustrom::node::Node;
+use rustrom::protocol::{Init, Message};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum Payload {
+    Generate,
+    GenerateOk { id: String },
+}
+
+#[allow(dead_code)]
+struct UniqeIdNode {
+    id: String,
+    msg_id: u32,
+}
+
+impl Node<Payload> for UniqeIdNode {
+    fn from_init(init: Init) -> Self
+    where
+        Self: Sized,
+    {
+        Self {
+            id: init.node_id,
+            msg_id: 0,
+        }
+    }
+
+    fn step(&mut self, input: Message<Payload>, mut output: &mut StdoutLock) -> anyhow::Result<()> {
+        let payload = match input.body.payload {
+            Payload::Generate => Payload::GenerateOk {
+                id: Uuid::new_v4().to_string(),
+            },
+            Payload::GenerateOk { .. } => unreachable!(),
+        };
+
+        let reply = input.into_reply_with_payload(Some(&mut self.msg_id), payload);
+
+        reply.send(&mut output)
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop::<UniqeIdNode, _>()
 }
